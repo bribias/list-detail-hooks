19d20a2f1235e5ad2ce4e0b84dae52ea6a4146d2
Brianna Bias <brianna.bias@gmail.com>
2021-09-19 17:41:31 -0700

    character list component pre-hooks

--- src/components/characters/CharacterList.jsx
+++ src/components/characters/CharacterList.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Character from './Character';
+import Character from '../characters/Character';
 
 const CharacterList = ({ characters }) => {
-    
+    return (
+        <ul aria-label="characters" role="list">
+        {characters.map((character) => (
+            <li key={character.id}>
+                <Character
+                    name={character.name}
+                    image={character.image}
+                />
+            </li>
+        ))}
+        </ul>
+    )
 }
+
+CharacterList.propTypes = {
+    characters: PropTypes.arrayOf(
+        PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired
+    })
+    ).isRequired,
+};
+
+export default CharacterList;
